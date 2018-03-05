12dec1b75dad5d69ccb8bf1126c4b6e2dc55104a
Krixium <bennywang75@hotmail.com>
2018-03-05 12:02:59 -0800

    Fully commented clnt.c

--- clnt.c
+++ clnt.c
@@ -1,6 +1,51 @@
+/*------------------------------------------------------------------------------------------------------------------
+-- SOURCE FILE:         clnt.c - A client server application that utilizes message queues.
+--
+-- PROGRAM:             assign2
+--
+-- FUNCTIONS:
+--                      int clnt(const int qid, const int priority, const char * filename)
+--
+--
+-- DATE:                March 5, 2018
+--
+-- REVISIONS:           N/A
+--
+-- DESIGNER:            Benny Wang
+--
+-- PROGRAMMER:          Benny Wang
+--
+-- NOTES:
+-- This file contains all the code for the client.
+----------------------------------------------------------------------------------------------------------------------*/
 #include "clnt.h"
 
-
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION:            clnt
+--
+-- DATE:                March 5, 2018
+--
+-- REVISIONS:           N/A
+--
+-- DESIGNER:            Benny Wang
+--
+-- PROGRAMMER:          Benny Wang
+--
+-- INTERFACE:           int clnt (const int qid, const int priority, const char * filename)
+--                          const int qid: The message queue id of the server.
+--                          const int priority: The requested priority.
+--                          const char * filename: The name of the requested file.
+--
+-- RETURNS:             The exit code.
+--
+-- NOTES:
+-- This is the main entry point for the client. The client performs the following:
+--
+-- 1) Collect the requsted priority, filename, and pid of itself and sends it to the server.
+-- 2) Listen for a response from the server. If the response is an error then the client exits.
+-- 3) Listen for messages as long as the incoming messages are full message and display them to stdout.
+--      Note: The client only listens for messages where the type is its pid.
+----------------------------------------------------------------------------------------------------------------------*/
 int clnt(const int qid, const int priority, const char * filename)
 {
     int pid;
